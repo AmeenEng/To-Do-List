25a7c11757666b333ed7f3b4f754a3e638677579
Ameen Alalimi <ameenalalimi.dev@gmail.com>
2025-09-24 22:08:36 +0300

    Add task creation and display functionality using PDO

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 require 'db.php';
+
+if(isset($_POST["addtask"]) && isset($_POST["task"])) {
+    $task = $_POST["task"];
+    $conn -> query("INSERT INTO tasks (task) VALUES ('$task')");
+    header("Location: index.php");
+    exit();
+}
+
+$result = $conn->query("SELECT * FROM tasks ORDER BY id DESC");
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +26,13 @@ require 'db.php';
             <input type="text" name="task" id="" placeholder="Enter new task: ">
             <button type="submit" name="addtask">ADD</button>
         </form>
+        <ul>
+            <?php while($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+                <li>
+                    <?php echo htmlspecialchars($row["task"]);?>
+                </li>
+            <?php endwhile; ?>
+        </ul>
     </div>
 </body>
 </html>
